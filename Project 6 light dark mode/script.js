1d96cff2854df3dc2bc9983f1d121542903df118
Sher-Bahadur-417 <sb1895218@gmail.com>
2025-10-29 23:36:34 +0500

    To confirm, type “delete my project”
    To confirm, type “delete my project”

---
+++ Project 6 light dark mode/script.js
@@ -0,0 +1,7 @@
+ // Dark/Light Mode
+        const modeToggle = document.getElementById("mode-toggle");
+        modeToggle.addEventListener("click", () => {
+            document.body.classList.toggle("dark-mode");
+            modeToggle.textContent = document.body.classList.contains("dark-mode") ? "☀️" : "🌙";
+        });
+
