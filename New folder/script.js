2f51a0dba1513dbefd5051551014572dc47657e4
Sher Bahadur <sb1895218@gmail.com>
2025-10-31 14:27:40 +0500

    Delete New folder directory

--- New folder/script.js
+++
@@ -1,7 +0,0 @@
-// 🌙 Theme Toggle
-const themeBtn = document.getElementById('theme-btn');
-
-themeBtn.addEventListener('click', () => {
-  document.body.classList.toggle('light-mode');
-  themeBtn.textContent = document.body.classList.contains('light-mode') ? '🌞' : '🌙';
-});
